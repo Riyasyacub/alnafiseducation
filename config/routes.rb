cf2985dd4de7ed7f2cdd89a019464fa6dbb76dc0
riyasydckap <riyasy@dckap.com>
2022-01-23 22:00:46 +0530

    image&icons

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root 'pages#index'
-  get 'pages/service'
-  get 'pages/about_us'
-  get 'pages/enquiry'
-  get 'pages/contact'
+  get '/service' => 'pages#service'
+  get '/about_us' => 'pages#about_us'
+  get '/enquiry' => 'pages#enquiry'
+  get '/contact' => 'pages#contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
