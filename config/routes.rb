7b683e29bca9550cd2c7eef1c8b7bcae5eecd783
riyasydckap <riyasy@dckap.com>
2022-02-13 15:08:45 +0530

    message&admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
-  get 'bills/index'
-  get 'bills/form'
-  get 'bills/history'
-  get 'bills/print'
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  mount RailsAdmin::Engine => '/jafar', as: 'rails_admin'
   root 'pages#index'
   get '/service' => 'pages#service'
   get '/about_us' => 'pages#about_us'
   get '/enquiry' => 'pages#enquiry'
   get '/contact' => 'pages#contact'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   post '/message/create' => 'pages#message_create'
